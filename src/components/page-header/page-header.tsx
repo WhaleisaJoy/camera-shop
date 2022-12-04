82fa46e847e560cea900e427979a493b5fa43382
Maria <afterlifelineonomega@gmail.com>
2022-12-04 21:09:09 +0300

    Add basket index with products amount in the basket

--- src/components/page-header/page-header.tsx
+++ src/components/page-header/page-header.tsx
@@ -1,9 +1,13 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const';
+import { getCamerasInBasket } from '../../store/basket-data/selectors';
 import FormSearch from '../form-search/form-search';
 import MainNavList from '../main-nav-list/main-nav-list';
 
 function PageHeader(): JSX.Element {
+  const camerasInBasket = useSelector(getCamerasInBasket);
+
   return (
     <header className="header" id="header">
       <div className="container">
@@ -23,6 +27,7 @@ function PageHeader(): JSX.Element {
           <svg width="16" height="16" aria-hidden="true">
             <use xlinkHref="#icon-basket"></use>
           </svg>
+          { camerasInBasket.length > 0 && <span className="header__basket-count">{camerasInBasket.length}</span> }
         </Link>
       </div>
     </header>
